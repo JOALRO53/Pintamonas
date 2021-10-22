db810e497f5158b0a5ea0a16b76d969dcf20ea78
JOALRO53 <joalro2122daw2@protonmail.com>
2021-10-22 22:27:42 +0200

    Iniciat metode de dibuix del Rectangle

--- metodes.js
+++ metodes.js
@@ -1,18 +1,42 @@
-var posx,posy;
+var posx, posy;
+var queDibuixem = 0;
+punt1 = undefined;
 
-function position(event) {
+function position(event) 
+{
     posx = event.clientX;
     posy = event.clientY;
+    let info = document.getElementById('lbInfo');
+    if (punt1 != undefined && posx > 330) 
+    {
+        switch (queDibuixem) 
+        {
+            case 1: //Rectangle
+                info.innerHTML = "Seleccioneu el punt final"
+                break;
+        }
+    }
+
     //var mousePositions = document.getElementById('areagrafica');
     //mousePositions.innerHTML = 'X coords: ' + posx + ', Y coords: ' + posy;
 }
 
-function mouseClick(event){
-    
+function mouseClick(event) {
+    let mousePositions = document.getElementById('areagrafica');
+    let info = document.getElementById('lbInfo');
     mousePositions.innerHTML = 'X coords: ' + posx + ', Y coords: ' + posy;
-    if(posx < 170 && posy < 90)
+    if(!queDibuixem)
     {
-       let info = document.getElementById('lbInfo');
-       info.innerHTML = "Seleccinat Rectangle. Clickeu el punt inicial.";
+        if (posx < 170 && posy < 90) {
+            info.innerHTML = "Seleccionat Rectangle. Clickeu el punt inicial.";
+            queDibuixem = 1;
+        }
+    }
+    else
+    {
+        if (posx > 330 && punt1 == undefined)// Click en area grafica
+         {
+            punt1 = posx;
+         }
     }
 }
